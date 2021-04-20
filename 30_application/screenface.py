c2f295c27c0ec37a7ca54bbd43868e7527898086
Malcolm Smith Fraser <malcolmsfraser@gmail.com>
2021-04-19 21:42:57 -0400

    random change

--- 30_application/screenface.py
+++ 30_application/screenface.py
@@ -11,8 +11,11 @@ import tensorflow as tf
 import time
 
 model = tf.keras.models.load_model('../40_models/emotion-detection-model-A-opencv.h5')
+frame_trac = 0
+sent_log = {'Angry':0,'Disgust':0,'Fear':0,'Happy':0,'Sad':0,'Surprise':0,'Neutral':0}
 
 while True:
+	frame_trac += 1
 	cap = ImageGrab.grab()
 	start = time.time()
 	cap_np = np.array(cap)
@@ -25,16 +28,24 @@ while True:
 	sentiments = []
 	for face in croped:
 		sentiments.append(predict_sentiment(face,model))
-
 	box_faces(frame,faces,sentiments,size=0)
 	print(f"{len(faces)} faces detected.")
 	sent_dict = {x:sentiments.count(x) for x in sentiments}
-	print(sent_dict)
 
-	resize = cv2.resize(frame, (854,480))
 	end=time.time()
 	elapsed = end-start
-	print(f'Time elapsed: {elapsed :.4f}s')
+	print(f'Frame {frame_trac}, time elapsed: {elapsed :.4f}s')
+	# Get the 6 frame-average sentiment
+	for x in sent_dict.keys():
+		sent_log[x] += sent_dict[x]
+
+	if frame_trac%6 == 0:
+		for x in sent_log.keys():
+			sent_log[x] /= 6
+			sent_log[x] = round(sent_log[x],3)
+		print(sent_log)
+
+	resize = cv2.resize(frame, (854,480))
 	cv2.imshow('frame',resize)
 	if cv2.waitKey(1) == 27:
 		break
